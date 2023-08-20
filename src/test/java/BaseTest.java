38b906d95bf478490e5ccbf6fba8ed1116028800
manasamohan16 <manasa.mohan@testpro.io>
2023-08-20 11:49:41 -0400

    Homework17 with helper methods added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,88 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
+    public static WebDriver driver = null;
+    public static String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
+
         WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+    public void launchBrowser(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver= new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+
+@AfterMethod
+public void closeBrowser(){
+    driver.quit();
+}
+
+public void navigateToPage(){
+    driver.get(url);
+}
+public void provideEmail(String email){
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.clear();
+    emailField.sendKeys(email);
+}
+
+public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+}
+public void clickSubmit() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+        }
+    public void searchSong(String name) throws InterruptedException {
+        WebElement searchField= driver.findElement(By.cssSelector("input[type='search']"));
+        searchField.clear();
+        searchField.sendKeys(name);
+        Thread.sleep(2000);
+}
+public void viewAllSongs() throws InterruptedException {
+        WebElement viewAll = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
+        viewAll.click();
+    Thread.sleep(2000);
+    }
+    public void clickFirstSong() throws InterruptedException {
+        WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+        firstSong.click();
+        Thread.sleep(2000);
+    }
+    public void clickAddToButton() throws InterruptedException {
+        WebElement addToButton = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
+                addToButton.click();
+        Thread.sleep(2000);
+    }
+    public void selectPlaylist() throws InterruptedException {
+        WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'ManasaTest')]"));
+        playlist.click();
+        Thread.sleep(2000);
+    }
+    public String getNotificationMessage(){
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        System.out.println(notification.getText());
+        return notification.getText();
+    }
+
 }
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest {
+
+    @Test
+    public void addSongToPlaylist() throws InterruptedException {
+        String newSongAddedNotificationText = "Added 1 song into \"ManasaTest.\"";
+        navigateToPage();
+        provideEmail("manasa.mohan@testpro.io");
+        providePassword("te$t$tudent");
+        clickSubmit();
+searchSong("ketsa");
+viewAllSongs();
+clickFirstSong();
+clickAddToButton();
+selectPlaylist();
+Assert.assertEquals(getNotificationMessage(),newSongAddedNotificationText);
+    }
+
+    }
+
