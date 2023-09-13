6f24d6b5db4e55384564cfaf41473c780f6832f5
manasamohan16 <manasa.mohan@testpro.io>
2023-09-13 00:43:49 -0400

    Homework27

---
+++ src/test/java/Pages/HomePage.java
@@ -0,0 +1,20 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+    @FindBy(css = "img[class='avatar']")
+    WebElement avatarIcon;
+
+    public HomePage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+
+    public boolean isAvatarDisplayed() {
+        return findElement(avatarIcon).isDisplayed();
+    }
+
+
+}
